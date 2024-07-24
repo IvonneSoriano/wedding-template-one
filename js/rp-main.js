d3b0ca5a6e6b3eae4d3c09266ac1d1f916385db3
Ivonne Sanchez <ivonnesoriano25@gmail.com>
2024-07-24 15:53:01 -0600

    added song

--- js/rp-main.js
+++ js/rp-main.js
@@ -1,4 +1,8 @@
-$(document).ready(function(){const apiKey="AIzaSyBpsa2cQXnkT8cdwdj10V6D1QaM-jKbZSM";if($("body").hasClass("clemente-bojorquez")){$("#play-paused-button").click(function(e){const isPlaying=$(".album-art").hasClass("active");$(".album-art").toggleClass("active");$(".player-track").toggleClass("active");if(isPlaying){pauseSong()}else{playSong()}})}});function pauseSong(){$("#play-paused-button i").removeClass("fa-pause");$("#play-paused-button i").addClass("fa-play")}
-async function loadSong(apiKey){const res=await fetch(`https://www.youtube.com/watch?v=YwodhCjFbQ8${apiKey}`)
-console.log(res)}
+$(document).ready(function(){if($("body").hasClass("clemente-bojorquez")){$("#play-paused-button").click(function(e){const isPlaying=$(".album-art").hasClass("active");$(".album-art").toggleClass("active");$(".player-track").toggleClass("active");if(isPlaying){pauseSong()}else{playSong()}})}
+$("#backward-button").click(function(e){prevSong()})
+$("#forward-button").click(function(e){prevSong()})
+audio.addEventListener("timeupdate",updateProgress)});function updateProgress(e){const{duration,currentTime}=e.srcElement;const progressPercent=(currentTime/duration)*100;console.log(progressPercent);const progress=document.getElementById("progress");progress.style.width=`${progressPercent}%`}
+function pauseSong(){$("#play-paused-button i").removeClass("fa-pause");$("#play-paused-button i").addClass("fa-play");audio.pause()}
+async function loadSong(){const audio=document.getElementById("audio");audio.src=`../music/song.mp3`;console.log(res)}
 function playSong(){$("#play-paused-button i").removeClass("fa-play");$("#play-paused-button i").addClass("fa-pause");audio.play()}
+function prevSong(){loadSong();playSong()}
